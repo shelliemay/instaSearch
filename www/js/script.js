a6b732065f98a1352594bdc98b16135e1392575d
shelliemay <g1645131@cc.kyoto-su.ac.jp>
2016-10-31 23:41:19 +0900

    onsen

---
+++ www/js/script.js
@@ -0,0 +1,16 @@
+// This is a JavaScript file
+$(function() {
+
+$('#add-button').click(    
+    function(){
+        var count = $('li').length;
+        if(count < 5){
+            $('ul').append("<li><i class=\"material-icons prefix\">#</i><input id=\"icon_prefix\" type=\"text\" class=\"validate\" placeholder=\"タグ名\"></br></li>");
+        }
+        if(count > 3){
+            $('#add-button').css('display','none');
+        }
+    });
+});
+    
+    
