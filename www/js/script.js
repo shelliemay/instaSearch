e1bbaa489c3eb060ab4146cd3389f3bdc790c588
shelliemay <g1645131@cc.kyoto-su.ac.jp>
2016-11-02 14:39:09 +0900

    アニメーションつけました。

--- www/js/script.js
+++ www/js/script.js
@@ -1,16 +1,16 @@
 // This is a JavaScript file
 $(function() {
 
-$('#add-button').click(    
+$('#add-button').click(
     function(){
         var count = $('li').length;
         if(count < 5){
-            $('ul').append("<li><i class=\"material-icons prefix\">#</i><input id=\"icon_prefix\" type=\"text\" class=\"validate\" placeholder=\"タグ名\"></br></li>");
+            $('ul').append("<li class=\"animated fadeInUp\"><i class=\"material-icons prefix\">#</i><input id=\"icon_prefix\" type=\"text\" class=\"validate\" placeholder=\"タグ名\"></br></li>");
         }
         if(count > 3){
             $('#add-button').css('display','none');
         }
     });
-});
-    
+})
+
     
