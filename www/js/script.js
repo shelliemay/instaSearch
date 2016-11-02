b9be41a0679a82dbbf1e3787063247e503a5c027
shelliemay <g1645131@cc.kyoto-su.ac.jp>
2016-11-03 04:56:50 +0900

    next 戻るボタン。詳細ページを作成

--- www/js/script.js
+++ www/js/script.js
@@ -4,13 +4,16 @@ $(function() {
 $('#add-button').click(
     function(){
         var count = $('li').length;
-        if(count < 5){
+        if(count < 3){
             $('ul').append("<li class=\"animated fadeInUp\"><i class=\"material-icons prefix\">#</i><input id=\"icon_prefix\" type=\"text\" class=\"validate\" placeholder=\"タグ名\"></br></li>");
         }
-        if(count > 3){
+        if(count > 1){
             $('#add-button').css('display','none');
         }
     });
-})
+});
 
+function deback(){
+    window.location.href = 'search.html';
+}
     
